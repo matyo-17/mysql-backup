dcc82096533301ce8ff0da575ba145fc08e3a9d6
Teo Filson <filsonteo@gmail.com>
2024-07-17 12:00:26 +0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 DB_NAME="database_name"
-mysqldump --defaults-extra-file=/path/to/backup.cfg --single-transaction --quick $DB_NAME | gzip > /path/to/backup/file/$DB_NAME-$(date +%Y-%m-%d).sql.gz
+DIR_PATH="/path/to/backup/dir"
+
+mkdir -p $DIR_PATH/data
+mysqldump --defaults-extra-file=$DIR_PATH/backup.cfg --single-transaction --quick $DB_NAME | gzip > $DIR_PATH/data/$DB_NAME-$(date +%Y-%m-%d).sql.gz
